55990231e46c1bae2ccce40a472754fb8d0f3fd9
Mikhail Zhuravskiy <boodjoom@ukr.net>
2015-09-09 23:10:09 +0300

    initial project version

---
+++ GitTestSolution/GitTestSolution.cpp
@@ -0,0 +1,13 @@
+// GitTestSolution.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+  std::cout << "Hello git";
+  return 0;
+}
+
