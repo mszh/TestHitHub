958d601fa1797755a75bf39365879e54cd5a8f98
Mikhail Zhuravskiy <boodjoom@ukr.net>
2015-09-11 10:13:09 +0300

    resolve iss53

--- GitTestSolution/GitTestSolution.cpp
+++ GitTestSolution/GitTestSolution.cpp
@@ -10,6 +10,13 @@ int main()
   std::cout << "Hello git";
   std::cout << "\n New code for git";
   std::cout << "Test remove commit";
+
+  // only for iss53
+  for (int i = 0; i < 10; i++)
+  {
+    std::cout << "Iss53 i = " << i;
+    std::cout << std::endl;
+  }
   return 0;
 }
 
