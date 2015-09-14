e98ffb369fa7f5da082e0e12a81d9d3fa237ea50
Mikhail Zhuravskiy <boodjoom@ukr.net>
2015-09-14 14:40:10 +0300

    for testing new merge tool

--- GitTestSolution/GitTestSolution.cpp
+++ GitTestSolution/GitTestSolution.cpp
@@ -13,27 +13,12 @@ void ThreadTest()
 
 int main()
 {
-  static int i = 0;
-  i++;
-  std::cout << "Start main"<< std::endl;
-  std::cout << "i= " << i << std::endl;
-
   std::thread t(ThreadTest);
   t.join();
 
-  std::thread t2(main);
-  t2.join();
-
-  int a = 20;
-  // rebasing test
-  float c = 5.1 * 3;
-
-  // only for iss53
-  for (int i = 0; i < 10; i++)
-  {
-    std::cout << "Iss53 i = " << i;
-    std::cout << std::endl;
-  }
+  int k = 10;
+  
+  int b = 0;
 
   return 0;
 }
